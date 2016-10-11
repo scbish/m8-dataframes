63260e8363ff71f87862cd270c1eccaa2b9b304c
Steven Bishop <Steven@D-108-179-156-139.dhcp4.washington.edu>
2016-10-11 14:28:26 -0700

    added dolphins game

--- exercise-2/exercise.R
+++ exercise-2/exercise.R
@@ -1,10 +1,10 @@
 # Exercise 2: Creating data frames
 
 # Create a vector of the number of points the Seahawks have scored in each game this season (google "Seahawks")
-points.for <- c(3, 37, 27)
+points.for <- c(12, 3, 37, 27)
 
 # Create a vector of the number of points the Seahwaks have allowed to be scored against them in each game this season
-points.against <- c(9, 18, 17)
+points.against <- c(10, 9, 18, 17)
 
 # Combine your two vectors into a dataframe
 seahawks.points <- data.frame(points.for = points.for, points.against = points.against)
@@ -16,7 +16,7 @@ seahawks.points$diff <- seahawks.points$points.for - seahawks.points$points.agai
 seahawks.points$won <- seahawks.points$diff > 0
 
 # Create a vector of the opponents
-opponents <- c("Rams", "49ers", "Jets")
+opponents <- c("Dolphins", "Rams", "49ers", "Jets")
 
 # Assign your dataframe rownames of their opponents
 seahawks.points$opponents <- opponents
